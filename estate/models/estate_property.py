81f16b8a0237fb97d7e9c1dafb0606e1c992c227
Maximilian Le <mle@cwagmbh.de>
2023-11-09 13:41:12 +0100

    Squashed commit of the following:
    commit d25b981c3d23c9dd46699518b76acd7be107b178
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:39:46 2023 +0100
    
        added state field string, changed active and state field order
    
    commit 5e2fd11a937c2a7ec1ab0cfc6bf630e9676c63e4
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:33:05 2023 +0100
    
        changed default for active field, changed default value for state field
    
    commit b691f5bbdce2f885b33fafba045f3aba489a0078
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:27:17 2023 +0100
    
        added active and state fields to estate property
    
    commit eba07f4da002b1b0586694618593a137cb8bc57a
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:14:16 2023 +0100
    
        changed duration to 3 months from 6
    
    commit 0d43bfa374acc4e233670ece94c5cf8109dc6af4
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:10:19 2023 +0100
    
        added relativedelta libary
    
    commit 8fd4c731575d0a075693c8517ba1b9274eedaf15
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 13:03:01 2023 +0100
    
        changed availability date default
    
    commit d080c7b2f8af33ff10e88480adf1141d738f9702
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Thu Nov 9 12:41:50 2023 +0100
    
        add duration of month changed
    
    commit cb8703349773d686272e0df7e40f7da8a5bebbb1
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Wed Nov 8 13:46:59 2023 +0100
    
        added default availability data in 3 months
    
    commit 2a014e3418c9c7787e2387a50dbe16066ff43106
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Wed Nov 8 13:40:27 2023 +0100
    
        added more attributes to estate property fields
    
    commit 42aae758c5c7746ab93c23d7f5b83ae85dc584b2
    Author: Maximilian Le <mle@cwagmbh.de>
    Date:   Wed Nov 8 13:24:03 2023 +0100
    
        added odoo and data schema to xml files
    
    squash commit

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
@@ -10,10 +11,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,7 +22,28 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ],
         help="Orientation equals the direction of the garden."
     )
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')
+        ],
+        help="State of the estate",
+        required=True,
+        copy=False,
+        default='new'
+    )
+    active = fields.Boolean(default=True)
+
 
