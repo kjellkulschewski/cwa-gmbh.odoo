99caa289ebb7cf1e2b660284e238da99a91eee0b
darius <d.dao@cwahmbh.de>
2023-11-10 13:35:31 +0100

    Update zu Aufgabe 4 models

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,18 +2,28 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
-
+from dateutil.relativedelta import relativedelta
 
 class RecurringPlan(models.Model):
     _name = "estate_property"
     _description = "this is a estate_property example"
     _order = "sequence"
 
-    name = fields.Char('Plan Name', required=True, translate=True)
-    number_of_months = fields.Integer('# Months', required=True)
-    active = fields.Boolean('Active', default=True)
-    sequence = fields.Integer('Sequence', default=10)
-
-    _sql_constraints = [
-        ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    ]
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char(required=True)
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.float(required=True)
+    selling_price = fields.float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Boolean()
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[('north','North'),('east','East'),('south','South'),('west','West')],
+        help="Orientation is the direction from Point A to Point B.However it is always one of the four sides of the Horizon"
+    active = fields.Boolean('Active',readonly=True)
+    )
