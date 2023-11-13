ab9986ca7b0e77dce374501a2ad8c24ea49adb63
darius <d.dao@cwahmbh.de>
2023-11-13 11:46:51 +0100

    kapitel6_fixed

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,6 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menu.xml',
+        'views/estate_menus.xml',
     ]
 }
