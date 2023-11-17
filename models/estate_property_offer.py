a6982c9f880a2df67bb2046e59f9f3024495ec8c
Maximilian Le <m.le@cwagmbh.de>
2023-11-17 15:55:13 +0100

    latest changes (chapter 12)

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -27,6 +27,7 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", required=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one("estate.property.type", related="property_id.property_type_id", store=True)
 
     # computed
     @api.depends("create_date", "validity")
--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
@@ -14,3 +14,12 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
     sequence = fields.Integer("Sequence", default=1)
+    offer_ids = fields.One2many("estate.property.offer", inverse_name="property_type_id", string="Offers")
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            if record.offer_ids == self:
+                record.offer_count += 1
+
